e723b3aa308009c6c085b1638b0d21561ef0a97b
Molla7605 <130729219+Molla7605@users.noreply.github.com>
2024-03-30 23:12:24 +0900

    ...

--- OpenGLTemplate/Main.cpp
+++ OpenGLTemplate/Main.cpp
@@ -10,8 +10,7 @@ int main(void) {
         return -1;
 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -21,8 +20,7 @@ int main(void) {
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         return -1;
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
